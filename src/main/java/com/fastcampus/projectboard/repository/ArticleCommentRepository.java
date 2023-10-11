747a63fd892deaa73f01521d34973b237b6ed416
Sangddong <hehe74029@gmail.com>
2023-10-11 11:35:25 +0900

    commit

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -2,8 +2,6 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
---
+++ src/test/java/com/fastcampus/FastCampusProjectBoardApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fastcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+class FastCampusProjectBoardApplicationTests {	//간단한 크루드 테스트
+@Test
+    void contextLoads(){
+
+}
+}
